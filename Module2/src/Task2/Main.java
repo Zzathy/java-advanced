85b2949ba64107102cb53746f04e43f0dc8ab123
IzzaFathony <izzafathony27@gmail.com>
2022-10-01 20:53:26 +0700

    Module 2 Task 2 First Code(Errors)

---
+++ Module2/src/Task2/Main.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Task2;
+
+/**
+ *
+ * @author izzai
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        void cekName(char name), cekFrom(char from), cekDate(int date), list();
+
+        String book[10][100] = {{"This is Book, Malang. 2022"}};
+        String place[][10] = {{"Malang"}, {"Bandung"}, {"Surabaya"}};
+
+        void cekDate(int date) {
+            if(date < 2018 || date > 2022) {
+
+            }
+        }
+
+        void cekFrom(char from) {
+            for(int i = 0; place[i][0] != "\0"; i++) {
+                if(from.compareTo(place[i]) == 0) {
+                    return i;
+                }
+            }
+        }
+
+        void cekName(char name) {
+            for(int i = 0; book[i][0] != "\0"; i++) {
+                if(name.compareTo(book[i]) == 0) {
+                    return i;
+                }
+            }
+        }
+
+        void list() {
+            for(int i = 0; book[i][0] != "\0"; i++) {
+                System.out.println(book[i]);
+            }
+        }
+
+        public static void main(String[] args) {
+            char inName, inFrom, inDate;
+            int inIndex;
+
+            System.out.println("Welcome to Libary");
+            System.out.println("Nama : ");
+            inName = input.nextLine();
+            cekName(inName);
+            System.out.println("From : ");
+            inName = input.nextLine();
+            System.out.println("Date : ");
+            inDate = input.nextInt();
+            cekDate(inDate);
+            System.out.println(inName + "" + inFrom + "" + inDate);
+            book[1] = new String(name);
+            System.out.println("Buku berhasil ditambahkan");
+        }
+    }
+
+}
