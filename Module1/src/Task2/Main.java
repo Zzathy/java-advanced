115adf42bfd065e80a2c9abd301a92fb9c227119
IzzaFathony <izzafathony27@gmail.com>
2022-10-18 23:03:43 +0700

    Finish Task 1 Module 2

--- Module1/src/Task2/Main.java
+++ Module1/src/Task2/Main.java
@@ -14,11 +14,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int loop = 0;
+        String name, nim, email = "", phn;
+
+        int loop1 = 0;
+        int loop2 = 0;
+        int loop3 = 0;
+
+        System.out.print("Input Nama\t: ");
+        name = input.nextLine();
 
         do {
-            System.out.print("Input NIM : ");
-            String nim = input.nextLine();
+            System.out.print("Input NIM\t: ");
+            nim = input.nextLine();
+
             try {
                 if (nim.matches("[0-9]+")) {
                     if (nim.length() == 15) {
@@ -26,23 +34,62 @@ public class Main {
                         // if (nim.contains("10370311")) {
                         // substring have a chance of error if you input less than 4, 12 index
                         if (nim.substring(4, 12).equals("10370311")) {
-                            loop = 0;
-                            System.out.println("NIM Benar");
+                            loop1 = 0;
                         } else {
-                            loop = 1;
-                            throw new Exception("Kode NIM salah");
+                            loop1 = 1;
+                            throw new Exception("Kode NIM Salah");
                         }
                     } else {
-                        loop = 1;
-                        throw new Exception("Panjang NIM lebih/kurang dari 15");
+                        loop1 = 1;
+                        throw new Exception("Panjang NIM Lebih/Kurang Dari 15");
                     }
                 } else {
-                    loop = 1;
-                    throw new Exception("Inputan harus angka");
+                    loop1 = 1;
+                    throw new Exception("Inputan Harus Angka");
                 }
             } catch (Exception e) {
                 System.out.println(e);
             }
-        } while (loop == 1);
+
+            while (loop1 == 0 && loop2 == 0) {
+                try {
+                    System.out.print("Input Email\t: ");
+                    email = input.nextLine();
+                    if (email.matches("(.*)@webmail.umm.ac.id")) {
+                        loop2 = 1;
+                    } else {
+                        loop2 = 0;
+                        throw new Exception("Format Email Salah");
+                    }
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+
+            }
+
+            while (loop1 == 0 && loop2 == 1 && loop3 == 0) {
+                try {
+                    System.out.print("Input Tlp\t: ");
+                    phn = input.nextLine();
+                    if (phn.length() >= 10 && phn.length() <= 12) {
+                        loop3 = 1;
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                        System.out.println("\n====================");
+                        System.out.println("Nama\t : " + name);
+                        System.out.println("NIM\t : " + nim);
+                        System.out.println("Email\t : " + email);
+                        System.out.println("Tlp\t : " + phn);
+                    } else {
+                        loop3 = 0;
+                        throw new Exception("Panjang Nomor Telfon Salah");
+                    }
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+
+            }
+
+        } while (loop1 == 1);
     }
 }
