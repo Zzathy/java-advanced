1bc6f7ac24cd956acadc711b91d5f5ddb787d80a
IzzaFathony <izzafathony27@gmail.com>
2022-10-31 19:19:10 +0700

    Refactor method to make setter and getter

--- Module3/src/Task124/Balok.java
+++ Module3/src/Task124/Balok.java
@@ -2,13 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Task1;
+package Task124;
 
 /**
  *
  * @author izzai
  */
 public class Balok {
+    private int panjang, lebar, tinggi;
+
+    public int getPanjang() {
+        return panjang;
+    }
+
+    public void setPanjang(int panjang) {
+        this.panjang = panjang;
+    }
+
+    public int getLebar() {
+        return lebar;
+    }
+
+    public void setLebar(int lebar) {
+        this.lebar = lebar;
+    }
+
+    public int getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(int tinggi) {
+        this.tinggi = tinggi;
+    }
     protected void hasil() {
         Perhitungan ph = new Perhitungan();
         System.out.println("Hasil Luas Balok\t: " + ph.luas(this));
