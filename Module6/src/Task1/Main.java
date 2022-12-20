4509970c29a27477750ff9d81c0f87eb7965f49d
Izza Ihsan Fathony <izzafathony27@gmail.com>
2022-12-20 09:08:50 +0700

    Module6 finish

--- Module6/src/Task1/Main.java
+++ Module6/src/Task1/Main.java
@@ -1,56 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Task1;
+package Task1; // Package name
 
-//Import the Scanner library
-import java.util.Scanner;
+// Import library that app need
+import java.util.*;
+import java.io.*;
 
 /**
- *
  * @author Izza Ihsan Fathony | 202110370311297
  */
-public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
+    // Main method
     public static void main(String[] args) {
-//        Create a Scanner object
+        // Initialization variable for Scanner
         Scanner input = new Scanner(System.in);
-        
-        System.out.print("Input array length : ");
-        int length = input.nextInt();
-        int[] inNumber = new int[length];
-        String[] inString = new String[length];
-        
+
+        System.out.print("Input length : ");
+        int length = input.nextInt(); // Catch user input
+        String clean = input.nextLine(); // cleaning Scanner, because error from change data type
+
+        /**
+         * Inizialitation array variable but temporary
+         * length from input user
+         */
+        int[] inNumberTemp = new int[length];
+        String[] inStringTemp = new String[length];
+
+        // Iniziliation for index n length for inString n inNumber
         int countNumber = 0, countString = 0;
-        
-        int i = 1;
-        while(i <= length) {
+
+        // Input data process
+        for (int i = 0; i < length; i++) {
             System.out.print("Input data : ");
             String data = input.nextLine();
 
-            if(data.matches("[0-9]+")) {
+            // Check if input is number or string
+            if (data.matches("[0-9]+")) {
+                inNumberTemp[countNumber] = Integer.valueOf(data);
                 countNumber++;
-                for(int j = 0; j < countNumber; j++) {
-                    inNumber[j] = Integer.valueOf(data);
-                }
-            } else if(data.matches("[a-zA-Z]")) {
-                for(int k = 0; k < countNumber; k++) {
-                    inString[k] = data;
-                }
             } else {
-                i--;
+                inStringTemp[countString] = data;
+                countString++;
             }
         }
-        
-        for(int a = 0; a < length; a++) {
-            System.out.print("[" + a + "] = ");
-            System.out.print(inNumber[a] + ",");
+
+        // Inizialitation array variable real use
+        String[] inString = new String[countString];
+        int[] inNumber = new int[countNumber];
+
+        // Transfer data from temporary variable to real variable
+        for (int i = 0; i < countString; i++) {
+            inString[i] = inStringTemp[i];
         }
-        
+        for (int i = 0; i < countNumber; i++) {
+            inNumber[i] = inNumberTemp[i];
+        }
+
+        // Write File
+        try {
+            FileWriter writer = new FileWriter("input.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            // Write array to file
+            bufferedWriter.write("Angka: " + Arrays.toString(inNumber));
+            bufferedWriter.newLine();
+            bufferedWriter.write("String: " + Arrays.toString(inString));
+
+            bufferedWriter.close(); // Close file
+
+            System.out.println("Data berhasil disimpan ke dalam file input.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Read File
+        try {
+            File myObj = new File("input.txt");
+            Scanner myReader = new Scanner(myObj);
+            // Cek if file has text at nextline
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
-    
 }
